eb8d48a3c0ad21e34c91f2b26b856e53c45479e7
Ola <olawunmi263@yahoo.com>
2020-06-29 13:47:01 +0100

    Home Page Class created

--- src/test/java/co/uk/justeat/stepsDefinitions/SearchForRestaurantsStepDefinitions.java
+++ src/test/java/co/uk/justeat/stepsDefinitions/SearchForRestaurantsStepDefinitions.java
@@ -1,11 +1,14 @@
 package co.uk.justeat.stepsDefinitions;
 
 import co.uk.justeat.pages.BasePage;
+import co.uk.justeat.pages.HomePage;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.support.PageFactory;
 
 public class SearchForRestaurantsStepDefinitions  extends BasePage {
+    HomePage homePage = PageFactory.initElements(driver, HomePage.class);
 
     @Given("I navigate to Just Eat website")
     public void i_navigate_to_just_eat_website() {
